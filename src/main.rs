b574b015b8c0be1a76bf96a05911f8c0460e2686
vikas90244 <vs7811350@gmail.com>
2026-02-23 22:31:10 +0530

    fixed issue with binding

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,24 @@ mod router;
 mod handlers;
 
 use std::{env, net::SocketAddr};
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main(){
-
-
+async fn main() {
     let app = router::create_router();
-      let port: u16 = env::var("PORT")
+
+    let port: u16 = env::var("PORT")
         .unwrap_or_else(|_| "8080".to_string())
         .parse()
         .expect("PORT must be a number");
 
-    // Bind to 0.0.0.0
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
     println!("Listening on {}", addr);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:9000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
 
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
