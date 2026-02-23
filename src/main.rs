5faa75582dd713da1a980516f079c9a0d19d7d1e
vikas90244 <vs7811350@gmail.com>
2026-02-23 21:57:32 +0530

    port binding logic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 mod router;
 mod handlers;
 
+use std::{env, net::SocketAddr};
 
 #[tokio::main]
 async fn main(){
 
-    tokio::fs::create_dir_all("../../uploads").await.unwrap();
 
-//     let app=Router::new().route("/", get(|| async {"Hello, world"} ));
     let app = router::create_router();
+      let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
+    // Bind to 0.0.0.0
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+
+    println!("Listening on {}", addr);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:9000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
